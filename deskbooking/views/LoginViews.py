202c93126d7431f201b11d5bced22030212ca9db
Ioanni <66422037+Kygo0@users.noreply.github.com>
2023-04-06 14:17:43 +0300

    User is redirected to deskbooking page
    If users inputs correct credentials they are redirected to the desk-booking page where they can book an available desk.

--- deskbooking/views/LoginViews.py
+++ deskbooking/views/LoginViews.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from ..forms import LoginForm
 from django.contrib import messages
+from ..forms import LoginForm
+
 
 def user_login(request):
-    if request.method == "GET":
+    if request.method == 'GET':
         form = LoginForm()
         return render(request, 'account.html', {'form': form})
     elif request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    messages.success(request, 'You have successfully logged in')
-                    return redirect('login')
+                    messages.success(request, 'Login Successful')
+                    return redirect('deskbook')
                 else:
-                    messages.error(request, 'Your account is disabled')
-                    return redirect('login')
+                    messages.error(request, 'Disabled account')
             else:
                 messages.error(request, 'Invalid login credentials')
-                return redirect('login')
         else:
             form = LoginForm()
-            return render(request, 'account.html', {'form': form})
+        return render(request, 'account.html', {'form': form})
+
+
+def deskbook(request):
+    desks = [1, 2, 3]
+    return render(request, 'deskbook.html', {'desks': desks})
